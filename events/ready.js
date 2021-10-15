370f9b42997aa3472c6822b2d9accd6b015e9c5f
LeFearr <78447253+LeFearr@users.noreply.github.com>
2021-10-15 23:19:15 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ const log = message => {
 module.exports = async client => {
   
 client.user.setActivity(`${config.prefix}help | .play -
-BOT: Şu an ` + client.channels.cache.size + ` adet kanala, ` + client.guilds.cache.size + ` adet sunucuya ve ` + client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`, {
+BOT: Şu an ` + client.guilds.cache.size + ` adet sunucuya ve ` + client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`, {
 type: "PLAYING",})
     log(`[BOT] Aktif, Komutlar Yüklendi.`)
   }
